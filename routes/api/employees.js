711ef484f8e5e6428c1f56b76614a0d89b494dc5
Saurav Rathod <sauravrathod23@gmail.com>
2024-04-02 14:10:18 +0530

    MCV Api updates

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,31 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const data = {};
-data.employees = require("../../data/employees.json");
-
+const employeeController = require("../../controllers/employeeController");
 router
   .route("/")
-  .get((req, res) => {
-    res.json(data.employees);
-  })
-  .post((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
-  })
-  .put((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
-  })
-  .delete((req, res) => {
-    res.json({ id: req.body.id });
-  });
+  .get(employeeController.getAllEmployees)
+  .post(employeeController.createNewEmployee)
+  .put(employeeController.updateEmployee)
+  .delete(employeeController.deleteEmployee);
 
-router.route("/:id").get((req, res) => {
-  res.json({ id: req.params.id });
-});
+router.route("/:id").get(employeeController.getEmployee);
 
 module.exports = router;
